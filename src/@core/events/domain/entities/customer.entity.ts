d20d139fa543288d16e65a60d6fc74fe982d6a48
vaguiiinho <vaguiiinho5@gmail.com>
2024-01-26 17:38:42 -0300

    abstracoes entity and aggregateRoot

--- src/@core/events/domain/entities/customer.entity.ts
+++ src/@core/events/domain/entities/customer.entity.ts
@@ -1,15 +1,18 @@
+import { AggregateRoot } from "@/@core/commom/domain/aggregate-root"
+
 export type CustomerConstructorProps = {
     id?: string
     cpf: string
     name: string
 }
 
-export class Customer {
+export class Customer extends AggregateRoot {
     id: string
     cpf: string
     name: string
 
     constructor(props: CustomerConstructorProps) {
+        super()
         this.id = props.id
         this.cpf = props.cpf
         this.name = props.name
