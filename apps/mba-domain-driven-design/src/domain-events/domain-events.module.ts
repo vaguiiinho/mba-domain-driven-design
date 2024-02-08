34c0764eef8351154728678d2b001d5326c0823b
vaguiiinho <avanlendario@gmail.com>
2024-02-07 23:52:44 -0300

    Event sourcing e replay dos agregados

--- apps/mba-domain-driven-design/src/domain-events/domain-events.module.ts
+++ apps/mba-domain-driven-design/src/domain-events/domain-events.module.ts
@@ -1,13 +1,37 @@
-import { Global, Module } from '@nestjs/common';
+import { Global, Module, OnModuleInit } from '@nestjs/common';
 import { DomainEventManager } from '../@core/common/domain/domain-event-manager';
 import { IntegrationEventsPublisher } from './integration-events.publisher';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { StoredEventSchema } from '../@core/stored-events/infra/db/schemas';
+import { StoredEventMysqlRepository } from '../@core/stored-events/infra/db/repositories/stored-event-mysql.repository';
+import { EntityManager } from '@mikro-orm/mysql';
+import { ModuleRef } from '@nestjs/core';
+import { IDomainEvent } from '../@core/common/domain/domain-event';
 
 @Global()
 @Module({
+    imports: [MikroOrmModule.forFeature([StoredEventSchema])],
     providers: [
         DomainEventManager,
         IntegrationEventsPublisher,
+        {
+            provide: 'IStoredEventRepository',
+            useFactory: (em) => new StoredEventMysqlRepository(em),
+            inject: [EntityManager],
+        },
     ],
     exports: [DomainEventManager],
 })
-export class DomainEventsModule { }
+export class DomainEventsModule implements OnModuleInit {
+    constructor(
+        private readonly domainEventManager: DomainEventManager,
+        private moduleRef: ModuleRef,
+    ) { }
+
+    onModuleInit() {
+        this.domainEventManager.register('*', async (event: IDomainEvent) => {
+            const repo = await this.moduleRef.resolve('IStoredEventRepository');
+            await repo.add(event);
+        });
+    }
+}
--- apps/mba-domain-driven-design/src/mikro-orm.config.ts
+++ apps/mba-domain-driven-design/src/mikro-orm.config.ts
@@ -1,26 +1,28 @@
 import {
+  CustomerSchema,
+  EventSchema,
+  EventSectionSchema,
+  EventSpotSchema,
+  OrderSchema,
+  PartnerSchema,
+  SpotReservationSchema,
+} from './@core/events/infra/db/schemas';
+import { StoredEventSchema } from './@core/stored-events/infra/db/schemas';
+
+export default {
+  entities: [
+    PartnerSchema,
     CustomerSchema,
     EventSchema,
     EventSectionSchema,
     EventSpotSchema,
     OrderSchema,
-    PartnerSchema,
     SpotReservationSchema,
-  } from './@core/events/infra/db/schemas';
-  
-  export default {
-    entities: [
-      PartnerSchema,
-      CustomerSchema,
-      EventSchema,
-      EventSectionSchema,
-      EventSpotSchema,
-      OrderSchema,
-      SpotReservationSchema,
-    ],
-    dbName: 'events',
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    type: 'mysql',
-  };
+    StoredEventSchema,
+  ],
+  dbName: 'events',
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  type: 'mysql',
+};
